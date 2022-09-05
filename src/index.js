5653e4bfd119a2b0325c478a1c34a09d3cd5c46c
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2022-09-05 14:16:09 +0100

    React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function CreditCard({ cardInfo }) {
+  cardInfo = {
+    name: "TEMILOLUWA J. ONALEYE",
+    number: 1234567887654321,
+    bank: "Big Bank, Inc.",
+    expiry: "08 / 19",
+  };
+  return (
+    <div className="creditCard">
+      <div className="creditCard__header">
+        <header>
+          <h1>{cardInfo.bank}</h1>
+        </header>
+      </div>
+      <div className="creditCard__number">
+        <span>1234</span>
+        <span>5678</span>
+        <span>8765</span>
+        <span>4321</span>
+        <br />
+        <small className="creditCard__numberSmall">1234</small>
+        <p>
+          <small>VALID</small>
+          <br />
+          <small>THRU</small>
+          {cardInfo.expiry}
+        </p>
+      </div>
+      <div className="creditCard__name">{cardInfo.name}</div>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<CreditCard cardInfo />, document.querySelector("#root"));
